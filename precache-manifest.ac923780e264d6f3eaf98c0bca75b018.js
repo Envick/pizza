33126c82e1b825ae83002e2ce0b5ad626bce068d
Envick <62522199+Envick@users.noreply.github.com>
2020-06-09 02:32:43 +0300

    Updates

---
+++ precache-manifest.ac923780e264d6f3eaf98c0bca75b018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38aad7970ba394b7f6d12edd04ec072c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "286d61198b8a5283f3e9",
+    "url": "/static/css/main.cde84d83.chunk.css"
+  },
+  {
+    "revision": "cb02b65546a997fe3539",
+    "url": "/static/js/2.46e74ac0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46e74ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286d61198b8a5283f3e9",
+    "url": "/static/js/main.216bafe9.chunk.js"
+  },
+  {
+    "revision": "985cab6dc52a4dc8e646",
+    "url": "/static/js/runtime-main.055154e0.js"
+  }
+]);
