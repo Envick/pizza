21530b6b99837105f717b562f4fe14bd105af7ce
Envick <62522199+Envick@users.noreply.github.com>
2020-06-09 11:31:39 +0300

    Updates

---
+++ precache-manifest.2fc50e02f1553217c6ba6b5738098185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b2082fa534d0205b640c51f45e75f4",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "a27337dfecc68e08dc9f",
+    "url": "/pizza/static/css/main.cde84d83.chunk.css"
+  },
+  {
+    "revision": "cb02b65546a997fe3539",
+    "url": "/pizza/static/js/2.46e74ac0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza/static/js/2.46e74ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27337dfecc68e08dc9f",
+    "url": "/pizza/static/js/main.f5fd7fd3.chunk.js"
+  },
+  {
+    "revision": "62e283adcea50fa43181",
+    "url": "/pizza/static/js/runtime-main.044bcf99.js"
+  }
+]);
